db45b99d2e112af9ef1e60246ccee0a7c32e99c7
John Bampton <jbampton@users.noreply.github.com>
2018-01-01 13:12:39 +1000

    Add missing closing </li> HTML tag

--- index.rb
+++ index.rb
@@ -172,7 +172,7 @@ $page = add_links(page_count)
 page_build(page_count)
 # restart common page region
 $page = %(
-            <li class="nuchecker"><a target="_blank" rel="noopener">Valid HTML</a>
+            <li class="nuchecker"><a target="_blank" rel="noopener">Valid HTML</a></li>
         </ul>
         <a href="http://s05.flagcounter.com/more/BHT" target="_blank" rel="noopener">
           <img src="http://s05.flagcounter.com/count2/BHT/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_250/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter">
